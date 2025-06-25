563ff5d9b96af38f7232f2843331a617dee45f8f
zach-lawrence-1 <mine268253@gmail.com>
2025-06-25 13:45:46 -0500

    notification bell now dynamically shows notification amount

--- src/components/DonorHeader.tsx
+++ src/components/DonorHeader.tsx
@@ -1,3 +1,5 @@
+import { buildQueries } from "@testing-library/react";
+
 export const HeaderBar: React.FC<{}> = () => {
     let username = "Seraph M.";
     return (
@@ -7,7 +9,7 @@ export const HeaderBar: React.FC<{}> = () => {
             <input className="search-bar" placeholder="Search items, collections, and accounts" />
             <div className="user-stuff">
                 <button className="moon"><img src="/dark_mode.svg" alt="moon" /></button>
-                <button className="notification"><img src="/notifications.svg" alt="bell" /></button>
+                <NotificationBell />
                 <div className="profile">
                     <div className="profile-picture">
                         <img src="main profile.png" alt="profile picture" />
@@ -21,4 +23,19 @@ export const HeaderBar: React.FC<{}> = () => {
         </header>
     </>
     );
+}
+
+const NotificationBell: React.FC<{}> = () => {
+    const notifications = ["notification 1", "notification 2"];
+    if (notifications.length > 0)
+    {
+        return (
+        <>
+            <button className="notification"><img src="/notifications.svg" alt="bell" /></button>
+            <div className="notification-amount">{notifications.length}</div>
+        </>
+        );
+    }
+
+    return <button className="notification"><img src="/notifications.svg" alt="bell" /></button>;
 }
