efb69fe3a120c21ebdf2be2a982fe3802130445b
zach-lawrence-1 <mine268253@gmail.com>
2025-07-28 16:14:37 -0500

    made yourProposals page look like design, added props

--- src/components/ProposalList.tsx
+++ src/components/ProposalList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ProposalList.css';
 
 //TODO: make the table data into props
-const ProposalList: React.FC = () => {
+const ProposaList: React.FC = () => {
   return (
     <div className="proposal-list">
       <div className="proposal-table">
@@ -52,5 +52,48 @@ const ProposalList: React.FC = () => {
     </div>
   );
 };
+
+interface ProposalListData {
+  name: string;
+  blockchain: string;
+  author: string;
+  authorIcon: string;
+  price: number;
+  image: string;
+}
+
+interface ProposalList {
+  proposalList: ProposalListData[];
+}
+
+const ProposalList: React.FC<ProposalList> = ({proposalList}) => {
+  return (
+    <div className="proposal-list">
+      <div className="proposal-table">
+        <div className="proposal-table-header">
+          <div>Name</div>
+          <div>Blockchain</div>
+          <div>Author</div>
+          <div>Price</div>
+        </div>
+        {proposalList.map((item, index) => (
+          <CreateProposalRows key={index} name={item.name} blockchain={item.blockchain} author={item.author} authorIcon={item.authorIcon} price={item.price} image={item.image} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const CreateProposalRows: React.FC<ProposalListData> = ({name, blockchain, author, authorIcon, price, image}) => {
+  return (
+    <div className="proposal-table-row">
+      <div className="proposal-table-item"><div><img className="proposal-table-project-image" src={image} alt={name + " icon"} />{name}</div></div>
+      <div className="proposal-table-item"><div><img className="proposal-table-blockchain-image" src="/Solana.svg" alt="solana icon" />{blockchain}</div></div>
+      <div className="proposal-table-item"><div><img className="proposal-table-author-image" src={authorIcon} alt={author} />{author}</div></div>
+      <div className="proposal-table-item">{price} Sol</div>
+    </div>
+  );
+}
+
 
 export default ProposalList;
--- src/pages/YourProposalsPage.tsx
+++ src/pages/YourProposalsPage.tsx
@@ -3,11 +3,61 @@ import MainFooter from "../components/MainFooter";
 import MainHeader from "../components/MainHeader";
 import MainSideBar, { MainPageToLoad } from "../components/MainSideBar";
 import ProposalList from "../components/ProposalList";
+import YourCollections from "./YourCollectionsPage";
 import "./YourProposalsPage.css";
 
 //NOTE: may need clarification on what this page is for
 const YourProposals: React.FC<{}> = () => {
-    const YourProposals = [{}];
+    const YourProposals = [
+        {
+            name: "Election Season",
+            blockchain: "Solana",
+            author: "Ivan Morais",
+            authorIcon: "/Matilda Profile.png",
+            price: 0.45,
+            image: "/election season.png"
+        },
+        {
+            name: "Lorr's Ad Issues",
+            blockchain: "Solana",
+            author: "Langke Zambo",
+            authorIcon: "/Matilda Profile.png",
+            price: 5.13,
+            image: "/Lorr.png"
+        },
+        {
+            name: "Venture Capitalist",
+            blockchain: "Solana",
+            author: "Cecilia Pozo",
+            authorIcon: "/Matilda Profile.png",
+            price: 3.59,
+            image: "/venture.png"
+        },
+        {
+            name: "Tracking on the Go",
+            blockchain: "Solana",
+            author: "Matilda Evans",
+            authorIcon: "/Matilda Profile.png",
+            price: 0.45,
+            image: "/tracking on the go.png"
+        },
+        {
+            name: "Behind One of Hacks",
+            blockchain: "Solana",
+            author: "Maeng Yeo-Jin",
+            authorIcon: "/Matilda Profile.png",
+            price: 3.40,
+            image: "/behind.png"
+        },
+        {
+            name: "Capturing Memories",
+            blockchain: "Solana",
+            author: "Lungelo Ngoaba",
+            authorIcon: "/Matilda Profile.png",
+            price: 3.40,
+            image: "/cube.png"
+        }
+    ];
     
     return (
         <>
@@ -19,7 +69,7 @@ const YourProposals: React.FC<{}> = () => {
                     YourProposals.length === 0 ?
                     <Empty emptyHeader="No proposals found" buttonText="Post your first NFT" navigationLink="/" />
                     :
-                    <ProposalList />
+                    <ProposalList proposalList={YourProposals}/>
                 }
             </div>
             <MainFooter />
